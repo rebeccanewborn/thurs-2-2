fbf782a5e207966ad04c6b9a538977ae72fedbf4
alexgriff <alexalexgriffith@gmail.com>
2017-02-02 15:07:51 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Profile from './Profile';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Profile />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <div className="profile-container">
+
+        </div>
+        <div className="button-container">
+
+          <button className="btn btn-warning btn-lg">
+            Click this Button
+          </button>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Counter;
